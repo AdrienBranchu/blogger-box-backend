daae90f8c5e8bfc6fd0885728fcfd9f470bdc474
adrienbranchu <adrienbranchu1@gmail.com>
2026-02-20 10:27:34 +0100

    Add database connection, Category entity and repository

---
+++ src/main/java/com/dauphine/blogger_box_backend/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.dauphine.blogger_box_backend.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dauphine.blogger_box_backend.models.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
